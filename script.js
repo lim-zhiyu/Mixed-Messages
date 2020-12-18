2aa70c65c5099d449bb762c44d0246a8ab16a594
zhiyu <limzhiyu570@gmail.com>
2020-12-19 01:24:18 +0800

    Add script.js and add hairColour to the generator object

---
+++ script.js
@@ -0,0 +1,8 @@
+const animeGirlGenerator = {
+    hairColour: ['black', 'brown', 'blonde', 'white', 'pink', 'blue', 'red', 'orange', 'green'],
+    hairStyle: ['loose long hair'],
+    eyeColour: [],
+    personality: []
+};
+
+// https://www.quora.com/What-is-the-meaning-of-the-different-hairstyles-in-anime
